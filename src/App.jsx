c63b0c85e141dc4e6a8e8fc5e15b50f2e94769a5
Aecio Levy <aeciolevy@gmail.com>
2017-03-07 00:48:35 +0000

    Added style and some HTML to test

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,24 @@ import React, {Component} from 'react';
 class App extends Component {
   render() {
     return (
-      <h1>Hello React :)</h1>
+      <div>
+        <nav className="navbar">
+        <a href="/" class="navbar-brand">Chatty</a>
+        <main class="messages">
+          <div class="message">
+            <span class="message-username">Anonymous</span>
+            <span class="message-content">I won't be impressed with technology until I can download food.</span>
+          </div>
+          <div class="message system">
+            Anonymous changed their name to nomnom.
+          </div>
+        </main>
+        <footer class="chatbar">
+          <input class="chatbar-username" placeholder="Your Name (Optional)" />
+          <input class="chatbar-message" placeholder="Type a message and hit ENTER" />
+        </footer>
+      </nav>
+      </div>
     );
   }
 }
